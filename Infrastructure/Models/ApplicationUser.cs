aed4411effbdc5f2320d4d39a37b9e02301b2547
viettrung2103 <44894608+viettrung2103@users.noreply.github.com>
2024-08-09 02:59:19 +0300

    install  microsoft.aspnetcore.identity so that applicationUser can inherit IdentityUser

---
+++ Infrastructure/Models/ApplicationUser.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//install with Nuget package
+using Microsoft.AspNetCore.Identity;
+
+
+namespace Infrastructure.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [DisplayName("First Name")]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [DisplayName("Last Name")]
+        public string? LastName { get; set; }
+
+        [DisplayName("Street Address")]
+        public string? StreetAddress { get; set; }
+
+        public string? City { get; set; }
+        public string? State { get; set; }
+        [DisplayName("Postal Code")]
+        public string? PostalCode { get; set; }
+        [NotMapped]
+        public string FullName { get { return FirstName + " " + LastName; } }
+    }
+
+}
